a33531024014c6e93af3db4afdfbd941dec9fe37
mikloslorinczi <miklos.lorinczi@bitrise.io>
2018-08-06 13:18:09 +0200

    test Bitrise build

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os/exec"
+	"strings"
 )
 
 var (
@@ -12,12 +14,26 @@ var (
 
 func main() {
 	flag.Parse()
-	fmt.Println("\nCommand Executor")
-	fmt.Println("Command:", command)
+	splitCommand := strings.Split(command, " ")
+	fmt.Println("\nCommand Executor\n")
+	fmt.Println("Full Command:", command)
+	fmt.Println("Base Command:", splitCommand[0])
 	fmt.Println("File:", outfile)
+	fmt.Println("Checking command ...")
+	fmt.Println("Check command :", checkCommand(splitCommand[0]))
 }
 
 func init() {
 	flag.StringVar(&command, "c", "", "Command to execute")
 	flag.StringVar(&outfile, "f", "outfile", "Otput file")
+}
+
+func checkCommand(command string) bool {
+	path, err := exec.LookPath(command)
+	if err != nil {
+		fmt.Println("Did not find command :", command)
+		return false
+	}
+	fmt.Printf("The command found in %s'\n", path)
+	return true
 }
