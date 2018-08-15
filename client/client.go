ed2746e08a7aef866a0d2052e77d540bfe04e49e
mikloslorinczi <miklos.lorinczi@bitrise.io>
2018-08-15 14:47:40 +0200

    Client name added

--- client/client.go
+++ client/client.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+var nodeName string
+
 func main() {
 
+	flag.Parse()
+
+	fmt.Printf("Hello, my name is %v\n", nodeName)
+
+}
+
+func init() {
+	flag.StringVar(&nodeName, "name", "", "Name of node")
 }
