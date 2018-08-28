14cef5a83beb6817f308fd8c91614cf950ce5195
mikloslorinczi <miklos.lorinczi@bitrise.io>
2018-08-28 16:25:18 +0200

    content of build.sh moved to bitrise.yml

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Building client container..."
-docker build -f iclient.Dockerfile -t infra_client .
-echo
-echo "Building server container..."
-docker build -f iserver.Dockerfile -t infra_server .
-echo
-echo "Finished"
